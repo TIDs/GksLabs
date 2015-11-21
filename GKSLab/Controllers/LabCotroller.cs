b8a462653ac572e50f5fb3fe8e08e2c93a88201b
Mersfast <merspower@gmail.com>
2015-11-21 13:21:33 +0200

    fix changes

--- GKSLab/Controllers/LabCotroller.cs
+++ GKSLab/Controllers/LabCotroller.cs
@@ -12,6 +12,7 @@ using GKSLab.Bussiness.Logic.Groups_Manager;
 using GKSLab.Bussiness.Logic.Modules_Manager;
 using GKSLab.Models.ViewModels;
 using GKSLab.Web.ExcelIOManager;
+using GKSLab.Bussiness.Entities.Graph;
 
 namespace GKSLab.Controllers
 {
@@ -137,17 +138,6 @@ namespace GKSLab.Controllers
             List<List<string>> inputData = new List<List<string>>();
             List<List<int>> groups = new List<List<int>>();
             List<List<int>> redistributionsGroup = new List<List<int>>();
-<<<<<<< HEAD
-            
-            //JUST TEST DATA
-            inputData.Add(new List<string>(7) { "T1", "C1", "F1", "F2", "T3", "T4" });
-            inputData.Add(new List<string>(4) { "T4", "C1", "F2" });
-            inputData.Add(new List<string>(6) { "T4", "F1",  "F2" });
-            inputData.Add(new List<string>(3) {  "T1", "F2" });
-            inputData.Add(new List<string>(6) { "T4", "F1", "T1", "T2", "C1", "F2" });
-            inputData.Add(new List<string>(5) { "T3", "F2", "T1", "T2", "C1" });
-            inputData.Add(new List<string>(4) { "T4", "T2", "T3", "C1" });
-=======
 
             ////First TEST DATA
             //inputData.Add(new List<string>(7) { "T1", "C1", "F1", "F2", "T3", "T4" });
@@ -167,20 +157,30 @@ namespace GKSLab.Controllers
             //inputData.Add(new List<string>(6) { "F2", "P1", "C1" });
             //inputData.Add(new List<string>(3) { "F1", "P1" });
 
->>>>>>> d6c4f5a441229320c966e3769edf77a3a1814d8d
             groups.Add(new List<int>() { 0, 1, 2, 3 });
 
+            var model = new List<HashSet<string>>();
             //creating graph
-            var graph = GraphManager.Create(groups[0], inputData);
+            //changing index for 0-based array
+            for (int index = 0; index < redistributionsGroup[0].Count; index++)
+            {
+                redistributionsGroup[0][index] -= 1;
+            }
+            var graphs = new List<Graph>();
+            foreach (var redistrItem in redistributionsGroup)
+            {
+                var list = new HashSet<string>();
+                var graph = GraphManager.Create(redistrItem, inputData);
+                list = GraphManager.CreateModules(graph, list);
+                model.Add(list);
+            }
+
             //Creating simplified graph model. It's should be like '[1->2,1->4,2->3]'
-            var joinedModel = graph.ToString();
-            GraphManager.FirstCasePack(graph);
-            return View("Test", model: joinedModel);
+            return View("Test", model: model.ToList());
         }
 
         public ActionResult Lab5()
         {
-<<<<<<< HEAD
             List<HashSet<string>> simplifyModules = new List<HashSet<string>>();
 
             // primary data
@@ -210,61 +210,8 @@ namespace GKSLab.Controllers
             simplifyModules = SimplifyModulesManager.SimplifyModules(groupsWithModules);
 
             ViewBag.PrimaryData = groupsWithModules;
+
             return View("Lab5", simplifyModules);
-=======
-            ComparationResult result;
-            var uniqueElements = 0;
-            List<List<string>> inputData = new List<List<string>>();
-            List<List<int>> groups = new List<List<int>>();
-            List<List<int>> redistributionsGroup = new List<List<int>>();
-            List<HashSet<string>> groupsWithStringElement;
-            //HttpPostedFileBase file = HttpContext.Request.Files[0];
-            try
-            {
-                inputData = ExcelReader.Read(file);
-                uniqueElements = ComparisonManager.UniqueElementsAmount(inputData);
-                result = ComparisonManager.CompareDetails(inputData);
-                groups = DevisionGroupsManager.CreateGroups(result);
-                groupsWithStringElement = RedistributionGroupsManager.CreateGroupsWithStringElement(inputData, groups);
-                var groupWithString = new List<HashSet<string>>(groupsWithStringElement);
-                var groupForRedistributions = new List<List<int>>();
-                groups.ForEach(x => groupForRedistributions.Add(x));
-                redistributionsGroup = RedistributionGroupsManager.RedistributionGroups(inputData, groupForRedistributions, groupWithString);
-            }
-            catch (Exception e)
-            {
-                string error = e.Message;
-                Debug.Write(e.Message);
-                return View(error);
-            }
-            //returning partial view
-            ViewBag.InputData = inputData;
-            ViewBag.Unique = uniqueElements;
-            ViewBag.Groups = groups;
-            ViewBag.GroupString = groupsWithStringElement;
-            ViewBag.RedistributedGroups = redistributionsGroup;
-            
-
-            groups.Add(new List<int>() { 0, 1, 2, 3 });
-            var model = new List<HashSet<string>>();
-            //creating graph
-            //changing index for 0-based array
-            for (int index = 0; index < redistributionsGroup[0].Count; index++)
-            {
-                redistributionsGroup[0][index] -= 1; 
-            }
-            var graphs = new List<Graph>();
-            foreach (var redistrItem in redistributionsGroup)
-            {
-                var list = new HashSet<string>();
-                var graph = GraphManager.Create(redistrItem, inputData);
-                list = GraphManager.CreateModules(graph, list);
-                model.Add(list);
-            }
-
-            //Creating simplified graph model. It's should be like '[1->2,1->4,2->3]'
-            return View("Test", model: model.ToList());
->>>>>>> d6c4f5a441229320c966e3769edf77a3a1814d8d
         }
     }
 }
