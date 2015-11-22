18063bd264d28e9a0331b3a5b3daa7e84f2fe320
svoychik <svoychik@gmail.com>
2015-11-23 00:38:16 +0200

    Links for graph method (lab4-..). Controller was cleaned

---
+++ GKSLab/Models/ViewModels/GraphLabViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GKSLab.Bussiness.Entities;
+
+namespace GKSLab.Models.ViewModels
+{
+    public class GraphLabViewModel
+    {
+        public List<List<int>> ResultingMatrix { get; set; }
+        public List<List<string>> InputData { get; set; }
+        public int Unique { get; set; }
+        public List<List<int>> Groups { get; set; }
+        public List<HashSet<string>> GroupString { get; set; }
+        public List<List<int>> RedistributedGroups { get; set; }
+    }
+}
---
+++ GKSLab/Models/ViewModels/СreationGraphModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GKSLab.Models.ViewModels
+{
+    public class СreationGraphModel
+    {
+        public IEnumerable<List<int>> Groups { get; set; }
+        public List<List<string>> InputData { get; set; }
+        public List<List<int>> RedistributedGroups { get; set; }
+    }
+}
