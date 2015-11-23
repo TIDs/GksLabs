1a1bab153b082e2f25fc23e2535658ae6eebbe7f
svoychik <svoychik@gmail.com>
2015-11-23 02:34:34 +0200

    controllers

--- GKSLab/Controllers/LabCotroller.cs
+++ GKSLab/Controllers/LabCotroller.cs
@@ -167,10 +167,7 @@ namespace GKSLab.Controllers
 
             var graphModel = new List<HashSet<string>>();
             //creating graph
-<<<<<<< HEAD
             if (!_init)
-=======
-<<<<<<< HEAD
             //for (int i = 0; i < _currentGraph.RedistributedGroups.Count; i++)
             //{
             //    for (int index = 0; index < _currentGraph.RedistributedGroups[0].Count; index++)
@@ -179,9 +176,7 @@ namespace GKSLab.Controllers
             //    }
             //}
            
-=======
             for (int i = 0; i < _currentGraph.RedistributedGroups.Count; i++)
->>>>>>> 54d73deaf51883ba9711bf42928ae1754b30cc58
             {
                 for (int i = 0; i < _currentGraph.RedistributedGroups.Count; i++)
                 {
@@ -193,7 +188,6 @@ namespace GKSLab.Controllers
                 _init = true;
             }
 
->>>>>>> 7fb64ad77ca6bcd152fd3d2a153d35f7bb80ff2a
             foreach (var redistrItem in _currentGraph.RedistributedGroups)
             {
                 var list = new HashSet<string>();
